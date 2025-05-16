2a3592c62cc65ea363310335953944dfa81e505f
JayeshKhare <97828199+1-Jammy@users.noreply.github.com>
2025-05-16 10:43:06 +0530

    Create EXP6_binaryTree.c

---
+++ EXP6_binaryTree.c
@@ -0,0 +1,80 @@
+//Experiment 6: Binary tree operations
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* create(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+Node* insert(Node* root, int value) {
+    if (root == NULL)
+        return create(value);
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else
+        root->right = insert(root->right, value);
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(Node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+    int choice, value;
+    while (1) {
+        printf("\n1. Insert\n2. Inorder\n3. Preorder\n4. Postorder\n5. Exit\nEnter choice: ");
+        scanf("%d", &choice);
+        if (choice == 5)
+            break;
+        switch (choice) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &value);
+                root = insert(root, value);
+                break;
+            case 2:
+                inorder(root);
+                break;
+            case 3:
+                preorder(root);
+                break;
+            case 4:
+                postorder(root);
+                break;
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}
