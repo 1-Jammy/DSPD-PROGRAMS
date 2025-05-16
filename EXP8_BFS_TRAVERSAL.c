63409fcd4ddb51d91ec052c46b270fd058c2eec7
JayeshKhare <97828199+1-Jammy@users.noreply.github.com>
2025-05-16 10:25:08 +0530

    Create EXP8_BFS_TRAVERSAL.c

---
+++ EXP8_BFS_TRAVERSAL.c
@@ -0,0 +1,87 @@
+//Experiment 8: BSF traversal using adjacency list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+typedef struct Node {
+    int vertex;
+    struct Node* next;
+} Node;
+
+typedef struct Graph {
+    int numVertices;
+    Node** adjLists;
+    int* visited;
+} Graph;
+
+Node* createNode(int v) {
+    Node* newNode = malloc(sizeof(Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Graph* createGraph(int vertices) {
+    Graph* graph = malloc(sizeof(Graph));
+    graph->numVertices = vertices;
+    graph->adjLists = malloc(vertices * sizeof(Node*));
+    graph->visited = malloc(vertices * sizeof(int));
+
+    for (int i = 0; i < vertices; i++) {
+        graph->adjLists[i] = NULL;
+        graph->visited[i] = 0;
+    }
+
+    return graph;
+}
+
+void addEdge(Graph* graph, int src, int dest) {
+    Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+void bfs(Graph* graph, int startVertex) {
+    int queue[MAX], front = 0, rear = 0;
+    graph->visited[startVertex] = 1;
+    queue[rear++] = startVertex;
+
+    while (front < rear) {
+        int currentVertex = queue[front++];
+        printf("%d ", currentVertex);
+
+        Node* temp = graph->adjLists[currentVertex];
+
+        while (temp) {
+            int adjVertex = temp->vertex;
+            if (!graph->visited[adjVertex]) {
+                graph->visited[adjVertex] = 1;
+                queue[rear++] = adjVertex;
+            }
+            temp = temp->next;
+        }
+    }
+}
+
+int main() {
+    Graph* graph = createGraph(6);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 1, 4);
+    addEdge(graph, 2, 4);
+    addEdge(graph, 3, 5);
+    addEdge(graph, 4, 5);
+
+    printf("BFS traversal starting from vertex 0:\n");
+    bfs(graph, 0);
+
+    return 0;
+}
